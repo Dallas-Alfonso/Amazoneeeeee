1e1b17332f219428e6d5e3c5f1410940a13ba900
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-09-26 15:41:02 -0400

    Progress up to Product Details

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name:'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpeg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '2',
+            name:'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpeg',
+            price: 80,
+            brand: 'Nike',
+            rating: 5.0,
+            numReviews: 7
+        },
+        {
+            _id: '3',
+            name:'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpeg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 80
+        },
+        {
+            _id:'4',
+            name:'Best Shoes',
+            category: 'Pants',
+            image: '/images/d1.jpeg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 80
+        }
+    ]
+}
