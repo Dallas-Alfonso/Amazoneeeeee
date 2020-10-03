6f8904e55b5615387412ce3c269bad2b5b1d5a7f
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-10-03 14:19:16 -0400

    error fixed from yesterday.  end of 11th portion

--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,9 +1,9 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LISt_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
 function productListReducer(state= {products: []}, action) {
 
     switch (action.type) {
-        case PRODUCT_LISt_REQUEST:
+        case PRODUCT_LIST_REQUEST:
             return {loading: true};
         case PRODUCT_LIST_SUCCESS:
             return {loading: false, products: action.payload };
