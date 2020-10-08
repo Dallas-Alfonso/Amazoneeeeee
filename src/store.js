b3ca8c6369732a7a92e95965d02c4285ab05238c
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-10-07 20:59:26 -0400

    pt.13 (less than halfway)

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
-
+import { cartReducer } from "./reducers/cartReducers"
 
 const initalState= {};
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSTION_COMPOSE__ || compose;
-const store = createStore(reducer, initalState, compose(applyMiddleware(thunk)));
+const store = createStore(reducer, initalState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
