7776bd7f5ebc514fbfcec64696757269eb188807
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-10-12 14:22:31 -0400

    just added delete button on cartscreen

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios"
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch) => {
 
@@ -14,10 +14,14 @@ const addToCart = (productId, qty) => async (dispatch) => {
                 countInStock: data.countInStock,
                 qty
             }
-        })
+        });
+
     } catch (error){
         
     }
 }
+const removeFromCart = (productId) => (dispatch) => {
+    dispatch ({ type: CART_REMOVE_ITEM, payload: productId });
+}
 
-export { addToCart }
+export { addToCart, removeFromCart }
