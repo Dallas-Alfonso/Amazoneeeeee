68c831fe7b13c4087cb8301557babbfc2948b959
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-10-21 13:26:42 -0400

    3 major screens errors.... will revisit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
-import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
 
 function App() {
+
+    const userSignin = useSelector(state => state.userSignin);
+    const {userInfo} = userSignin;
 
     const openMenu = () => {
         document.querySelector(".sidebar").classList.add("open");
@@ -23,12 +28,13 @@ function App() {
                         &#9776;
                     </button> 
                     <Link to="/">amazone</Link>
-                    
                 </div>
                 <div className="header-links">
                     <a href="cart.html">Cart</a>
-                    <Link to="/signin"></Link>
-                    <a href="signin.html">Sign In</a>
+                    {
+                        userInfo ? <Link to="/profile">{userInfo.name}</Link>:
+                        <Link to="/signin">Sign In</Link>       
+                    }
                 </div>
             </header>
             <aside className="sidebar">
@@ -42,6 +48,7 @@ function App() {
             <main className="main">
                 <div className="content">
                     <Route path="/signin" component={SigninScreen}/>
+                    <Route path="/register" component={RegisterScreen}/>
                     <Route path="/product/:id" component={ProductScreen}/>
                     <Route path="/cart/:id?" component={CartScreen} />
                     <Route path="/" exact={true} component={HomeScreen}/>
--- src/store.js
+++ src/store.js
@@ -1,19 +1,21 @@
 import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import Cookie from 'js-cookie';
-import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from "./reducers/cartReducers"
-import { userSigninReducer } from './reducers/userReducers'; 
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducers'; 
 
 const cartItems = Cookie.getJSON("cartItems")  || [];
+const userInfo = Cookie.getJSON("userInfo") || null;
 
-const initalState= { cart: { cartItems } };
+const initialState= { cart: { cartItems }, userSignin: {userInfo} };
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-    userSignin: userSigninReducer
+    userSignin: userSigninReducer,
+    userRegister: userRegisterReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSTION_COMPOSE__ || compose;
-const store = createStore(reducer, initalState, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
