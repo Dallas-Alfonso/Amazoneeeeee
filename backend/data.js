6746e32bec2c3a0f9f183d948ab5e98c82e58e84
Daniel Ebron <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-10-24 20:04:41 -0400

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name:'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpeg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 10 
+        },
+        {
+            _id: '2',
+            name:'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpeg',
+            price: 80,
+            brand: 'Nike',
+            rating: 5.0,
+            numReviews: 7,
+            countInStock: 10
+        },
+        {
+            _id: '3',
+            name:'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpeg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 80,
+            countInStock: 3 
+        },
+        {
+            _id:'4',
+            name:'Best Shoes',
+            category: 'Pants',
+            image: '/images/d1.jpeg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 80,
+            countInStock: 7 
+        }
+    ]
+}
